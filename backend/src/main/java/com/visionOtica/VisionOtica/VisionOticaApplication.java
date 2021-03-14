8a0251c49de5f853de0760a5389c692644bd4329
thiagosz63 <thiagosz63@gmail.com>
2021-03-14 14:32:34 -0300

    Project Created

---
+++ backend/src/main/java/com/visionOtica/VisionOtica/VisionOticaApplication.java
@@ -0,0 +1,13 @@
+package com.visionOtica.VisionOtica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionOticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionOticaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/visionOtica/VisionOtica/VisionOticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visionOtica.VisionOtica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionOticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
