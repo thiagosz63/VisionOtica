e97b98483d7515ec9dd4ab19670dead1cd7a33b7
thiagosz63 <thiagosz63@gmail.com>
2021-03-28 17:10:11 -0300

    Created Tabela de Produtos

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_product (name, price, image_Uri, description) VALUES ('Óculos SPEEDO', 253.00, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_bacon.jpg', 'SP1216 02B Azul Escuro Com Haste 360 Lente Tam 55.');
+INSERT INTO tb_product (name, price, image_Uri, description) VALUES ('Óculos Arnette', 400.00, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_moda.jpg', 'THE SEEKER AN7182 2702 Azul Striped Lente Tam 49.');
+
