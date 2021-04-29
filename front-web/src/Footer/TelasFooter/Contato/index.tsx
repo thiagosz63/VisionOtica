dae1e03b61511f232ecb58002d1e1ddfab05085f
Felipe Batista <felipedba2020@gmail.com>
2021-04-29 01:24:20 -0300

    update das Validações de Cadastro parte 1

--- front-web/src/Footer/TelasFooter/Contato/index.tsx
+++ front-web/src/Footer/TelasFooter/Contato/index.tsx
@@ -1,6 +1,24 @@
 import './style.css';
+import { ErrorMessage, Field, Form, Formik, FormikValues } from 'formik'
+import * as Yup from 'yup';
+import { pt } from 'yup-locale-pt';
+import axios from 'axios';
+
 
 function Contato() {
+    Yup.setLocale(pt);
+
+    const handleSubmit = (Values: FormikValues) => {
+        axios.post('http://localhost:8080', Values)
+    }
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
+        const validaCont = Yup.object().shape({
+        Nome: Yup.string().required(),
+        Email: Yup.string().email().required(),
+        Telefone: Yup.string().matches(phoneRegExp, 'O número de telefone não é válido.')
+    })
+
     return (
     <body >
         <div className='container '>
@@ -9,28 +27,39 @@ function Contato() {
                         Contato
                  </h2>
             </div>
-                <form className="col-md-6">
-                    <label>
-                            Nome <span>*</span>
-                        <input type="text" className="Contato-box" name="ContName" id="ContName" placeholder="Digite seu Nome" />
-                    </label>
-                    <label>
-                        Email <span>*</span>
-                    <input type="text" className="Contato-box" name="ContEmail"  id="ContEmail" placeholder="Digite seu e-mail" ></input>
-                    </label>
-                    <label>
-                        Telefone <span>*</span><br></br>
-                    <input type="text" name="ContTelefone" id="ContTelefone" className="contato-telefone" placeholder="(__) _____-_____"  /> 
-                    </label>
 
-                    <label htmlFor="mensagem">Tire suas dúvidas conosco <span>*</span> </label>
-                    <textarea className="contato-msn" id="mensagem" rows={8} cols={5} placeholder="Escreva sua Mensagem..."></textarea>
+            <Formik initialValues={{}} onSubmit={handleSubmit} validationSchema={validaCont}>
+                <Form className="col-md-6">
+                    <div>
+                       <label>Nome <span className="spanRed">*</span></label>
+                        <Field type="text" className="Contato-box" name="Nome" id="Nome" placeholder="Digite seu Nome" />
+                        <ErrorMessage className="spanRed" component='span' name='Nome'/>
+                    </div>
+
+                    <div>
+                    <label className="top-label">Email <span className="spanRed">*</span></label>
+                    <Field type="text" className="Contato-box" name="Email"  id="Email" placeholder="Digite seu e-mail" />
+                    <ErrorMessage className="spanRed" component='span' name='Email'/>
+                    </div>
+                    
+                    <div>
+                    <label className="top-label">Telefone <span className="spanRed">*</span><br></br></label>
+                    <Field type="text" name="Telefone" id="Telefone" className="contato-telefone" placeholder="(__) _____-_____"  /> 
+                    <ErrorMessage className="spanRed" component='span' name='Telefone'/>
+                    </div>
+
+                    <div>
+                    <label htmlFor="mensagem" className="top-label">Tire suas dúvidas conosco <span className="spanRed">*</span></label>
+                    <textarea className="contato-msn" name="Mensagem"  id="Mensagem"  rows={8} cols={5} placeholder="Escreva sua Mensagem..." />
+                    </div>
+
                     <div className="centralizar"> 
                         <input className="btn btn-info" type="submit" id="btnContEnviar" value="Enviar" />
                     </div>
-                </form>
-            </div>
-        </body>
+                </Form>
+            </Formik>
+        </div>
+    </body>
     )
 }
 export default Contato;
