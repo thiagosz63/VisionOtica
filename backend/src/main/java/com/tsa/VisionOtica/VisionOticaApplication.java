9c75651d90f69570d8e32ebb281316fe09b8777a
thiagosz63 <thiagosz63@gmail.com>
2022-06-06 20:57:37 -0300

    Project Created

---
+++ backend/src/main/java/com/tsa/VisionOtica/VisionOticaApplication.java
@@ -0,0 +1,13 @@
+package com.tsa.VisionOtica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionOticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionOticaApplication.class, args);
+	}
+
+}
